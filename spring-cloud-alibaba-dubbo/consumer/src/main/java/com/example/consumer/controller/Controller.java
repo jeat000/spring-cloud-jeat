f39d157e0bb2628615348c85ba010f4ec5d21823
jeat <jeat000@qq.com>
2021-02-05 11:52:59 +0800

    test

--- spring-cloud-alibaba-dubbo/consumer/src/main/java/com/example/consumer/controller/Controller.java
+++ spring-cloud-alibaba-dubbo/consumer/src/main/java/com/example/consumer/controller/Controller.java
@@ -5,6 +5,9 @@ import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * test
+ */
 @RestController
 public class Controller {
 
