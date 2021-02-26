e5dda814454f258189467875f68ea9214f461abe
jeat <jeat000@qq.com>
2021-02-26 14:17:12 +0800

    x

---
+++ Leecode-answer/Test/src/com/test/leecode/l_1178.java
@@ -0,0 +1,43 @@
+package com.test.leecode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class l_1178 {
+    public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
+        Map<Integer, Integer> frequency = new HashMap<Integer, Integer>();
+
+        for (String word : words) {
+            int mask = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word.charAt(i);
+                mask |= (1 << (ch - 'a'));
+            }
+            if (Integer.bitCount(mask) <= 7) {
+                frequency.put(mask, frequency.getOrDefault(mask, 0) + 1);
+            }
+        }
+
+        List<Integer> ans = new ArrayList<Integer>();
+        for (String puzzle : puzzles) {
+            int total = 0;
+            int mask = 0;
+            for (int i = 1; i < 7; ++i) {
+                mask |= (1 << (puzzle.charAt(i) - 'a'));
+            }
+            int subset = mask;
+            do {
+                int s = subset | (1 << (puzzle.charAt(0) - 'a'));
+                if (frequency.containsKey(s)) {
+                    total += frequency.get(s);
+                }
+                subset = (subset - 1) & mask;
+            } while (subset != mask);
+
+            ans.add(total);
+        }
+        return ans;
+    }
+}
