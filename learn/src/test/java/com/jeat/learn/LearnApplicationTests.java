cf7de908a6cb5c8ba98df43fc49da1bc76fff2cf
jeat <jeat000@qq.com>
2021-06-03 10:43:58 +0800

    20210603

---
+++ learn/src/test/java/com/jeat/learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeat.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
